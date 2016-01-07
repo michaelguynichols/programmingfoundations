98b6fb5e19a53b4187a8f9f39665681f7b745efb
michaelguynichols <michael.guy.nichols@gmail.com>
2016-01-07 22:43:10 +0100

    lesson 2 cleanup

--- smallprograms/calculator.rb
+++ smallprograms/calculator.rb
@@ -5,28 +5,111 @@
 # perform the operation on the two numbers
 # output the result
 
-puts "Please enter two numbers: "
+require 'yaml'
 
-Kernel.puts("Welcome to Calculator!")
+MESSAGES = YAML.load_file('calculator_messages.yml')
 
-Kernel.puts("What's the first number?")
-number1 = Kernel.gets().chomp()
-
-Kernel.puts("What's the second number?")
-number2 = Kernel.gets().chomp()
-
-Kernel.puts("What operation would you like to perform? 1) add 2) subtract 3) multiply 4) divide")
-operator = Kernel.gets().chomp()
-
-if operator == '1'
-  result = number1.to_i + number2.to_i
-elsif operator == '2'
-  result = number1.to_i - number2.to_i
-elsif operator == '3'
-  result = number1.to_i * number2.to_i
-else
-  result = number1.to_f / number2.to_f
+def prompt(message)
+  Kernel.puts("=> #{message}")
 end
 
+def valid_number?(num)
+  Float(num) || Integer(num) rescue false
+end
 
-Kernel.puts("The result is #{result}")
+def operation_to_message(num)
+  result = case num
+  when '1'
+    'Adding'
+  when '2'
+    'Subtracting'
+  when '3'
+    'Multiplying'
+  when '4'
+    'Dividing'
+  end
+  result
+end
+
+prompt(MESSAGES['welcome'])
+
+name = ''
+loop do
+  name = Kernel.gets().chomp()
+
+  if name.empty?()
+    prompt(MESSAGES['valid_name'])
+  else
+    break
+  end
+end
+
+prompt("Hi #{name}!")
+
+loop do # main loop
+  number1 = ''
+  loop do
+    prompt(MESSAGES['first_number'])
+    number1 = Kernel.gets().chomp()
+
+    if valid_number?(number1)
+      break
+    else
+      prompt(MESSAGES['invalid_number'])
+    end
+  end
+
+  number2 = ''
+  loop do
+    prompt(MESSAGES['second_number'])
+    number2 = Kernel.gets().chomp()
+
+    if valid_number?(number2)
+      break
+    else
+      prompt(MESSAGES['invalid_number'])
+    end
+  end
+
+  operator_prompt = <<-MSG
+  What operation would you like to perform?
+    1) add
+    2) subtract
+    3) multiply
+    4) divide
+  MSG
+
+  prompt(operator_prompt)
+
+  operator = ''
+  loop do
+    operator = Kernel.gets().chomp()
+
+    if %w(1 2 3 4).include?(operator)
+      break
+    else
+      prompt("Must choose 1, 2, 3, or 4")
+    end
+  end
+
+  prompt("#{operation_to_message(operator)} the two numbers...")
+
+  result =  case operator
+            when '1'
+              number1.to_i + number2.to_i
+            when '2'
+              number1.to_i - number2.to_i
+            when '3'
+              number1.to_i * number2.to_i
+            else
+              number1.to_f / number2.to_f
+            end
+
+  prompt("The result is #{result}")
+
+  prompt(MESSAGES['another_calculation'])
+  answer = Kernel.gets().chomp()
+  break unless answer.downcase().start_with?('y')
+end
+
+prompt(MESSAGES['thank_you'])
