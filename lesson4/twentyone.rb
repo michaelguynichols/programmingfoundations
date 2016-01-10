9d34766b62b41b57fc5d82877185db7af35786a2
michaelguynichols <michael.guy.nichols@gmail.com>
2016-01-10 23:06:32 +0100

    Updated 21

--- lesson4/twentyone.rb
+++ lesson4/twentyone.rb
@@ -11,11 +11,11 @@ def prompt(message)
   puts "=> #{message}"
 end
 
-def build_deck()
+def build_deck
   deck = {}
   SUITS.each do |suit|
     CARDS.each do |card|
-      if deck.has_key?(suit)
+      if deck.key?(suit)
         deck[suit] << card
       else
         deck[suit] = [card]
@@ -39,7 +39,6 @@ end
 
 def deal_card(deck_of_cards)
   dealt_card = nil
-  random_suit = nil
   while dealt_card == nil
     random_suit = SUITS.sample
     dealt_card = deck_of_cards[random_suit].sample
@@ -63,7 +62,7 @@ def display_cards(player, computer)
   system 'clear'
   c_cards = build_card_representations(computer[0, computer.length - 1])
   p_cards = build_card_representations(player[0, player.length - 1])
-  last_p_card = build_card_representations([player[-1]]).join()
+  last_p_card = build_card_representations([player[-1]]).join
   if computer.length > 2
     prompt "#{COMPUTER} has: #{c_cards.join(', ')}, and unknown card"
   else
@@ -74,12 +73,12 @@ def display_cards(player, computer)
   else
     prompt "#{PLAYER} have: #{p_cards.join(', ')} and #{last_p_card}"
   end
-  display_score(player, computer)
+  display_score(player)
 end
 
 def display_computer_hand(c_cards)
   copy_cards = build_card_representations(c_cards[0, c_cards.length - 1])
-  last_c_card = build_card_representations([c_cards[-1]]).join()
+  last_c_card = build_card_representations([c_cards[-1]]).join
   if c_cards.length > 2
     prompt "#{COMPUTER} had: #{copy_cards.join(', ')}, and #{last_c_card}"
   else
@@ -104,7 +103,7 @@ def score_cards(cards)
   score
 end
 
-def display_score(player, computer)
+def display_score(player)
   prompt "#{PLAYER}r cards are worth: #{score_cards(player)}"
 end
 
@@ -135,8 +134,8 @@ def final_result(player, computer)
   end
 end
 
-def computer_plays(deck_of_cards, p_cards, c_cards)
-  while score_cards(c_cards) < 17 || (score_cards(c_cards) < score_cards(p_cards) && score_cards(c_cards) < 17)
+def computer_plays(deck_of_cards, c_cards)
+  while score_cards(c_cards) < 17
     hit(deck_of_cards, c_cards)
   end
 end
@@ -169,7 +168,7 @@ loop do
 
   break if busted?(player_cards)
 
-  computer_plays(deck, player_cards, computer_cards)
+  computer_plays(deck, computer_cards)
 
   break if busted?(computer_cards) || who_won(player_cards, computer_cards)
 end
